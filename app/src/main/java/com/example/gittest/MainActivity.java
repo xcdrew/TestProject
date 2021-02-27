cca7e89cf36185c9e121800d11ccade488244a6e
xcdrew <xcdrewtb@Gmail.com>
2021-02-27 13:45:27 -0500

    Added Ernst

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("Hello");
         System.out.println("Hello Adil");
+        System.out.println("Hello Ernst");
     }
 }
